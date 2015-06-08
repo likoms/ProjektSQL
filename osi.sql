2f742065cc5af782b4b58998786e2c299dc71942
Piotr Trocki <pan.stroncky@gmail.com>
2015-06-08 10:22:48 +0200

    Test trigger

--- osi.sql
+++ osi.sql
@@ -9,12 +9,28 @@
 --  end loop;
 --end;
 
---DROP TABLE Dzial CASCADE CONSTRAINTS;
---DROP TABLE Towar  CASCADE CONSTRAINTS;
---DROP TABLE Pracownik CASCADE CONSTRAINTS;
---DROP TABLE Transakcja CASCADE CONSTRAINTS;
---DROP TABLE Opinie CASCADE CONSTRAINTS; 
---Drop table Log CASCADE CONSTRAINTS;
+DROP TABLE Dzial CASCADE CONSTRAINTS;
+DROP TABLE Towar  CASCADE CONSTRAINTS;
+DROP TABLE Pracownik CASCADE CONSTRAINTS;
+DROP TABLE Transakcja CASCADE CONSTRAINTS;
+DROP TABLE Opinia CASCADE CONSTRAINTS; 
+Drop table Log CASCADE CONSTRAINTS;
+
+ALTER TABLE Orders DROP CONSTRAINT fk_PerOrders;
+ALTER TABLE Towar DROP CONSTRAINT fk_towar; 
+ALTER TABLE Opinia DROP CONSTRAINT fk_opinia; 
+ALTER TABLE Pracownik DROP CONSTRAINT fk_pracownik; 
+ALTER TABLE Transakcja DROP CONSTRAINT fk_transakcja; 
+ALTER TABLE Transakcja DROP CONSTRAINT fk_transakcja_1; 
+
+DROP TRIGGER trigger_towar; 
+DROP TRIGGER trigger_pracownik;
+Drop trigger trigger_dzial;
+Drop trigger trigger_transakcja;
+Drop trigger trigger_opinia;
+
+
+
 
 CREATE TABLE Dzial
   (
@@ -30,7 +46,7 @@ CREATE TABLE Towar
     Nazwa VARCHAR2(25),
     Dzial NUMBER(7),
     Cena FLOAT(7),
-    Opinia NUMBER(7),
+   
     Ilosc  NUMBER(7)
   );
 CREATE TABLE Pracownik
@@ -48,8 +64,8 @@ CREATE TABLE Transakcja
     cena_jednostkowa FLOAT(7),
     Ilosc NUMBER(7),
     Suma FLOAT(7),
-    Sprzedawca NUMBER(7),
-    Opinia     NUMBER(7)
+    Sprzedawca NUMBER(7)
+   
   );
 
 CREATE TABLE Opinia
@@ -58,7 +74,8 @@ CREATE TABLE Opinia
     Ocena    NUMBER(1),
     Imie     VARCHAR2(25),
     Nazwisko VARCHAR2(25),
-    Tresc    VARCHAR2(50)
+    Tresc    VARCHAR2(50),
+    idT    NUMBER(7)
   );
   
 
@@ -78,12 +95,12 @@ INSERT INTO Dzial VALUES(6,'Papierosy i Alkohol',1 );
 
  --TOWAR(idT  Nazwa  Dzial   Cena   Opinia   Ilosc)  
 
-INSERT INTO TOWAR VALUES  (100, 'Zel pod prysznic', 1, 1.99, 1,10); 
-INSERT INTO TOWAR VALUES  (101, 'Cif', 2, 2.99, 2, 20 ); 
-INSERT INTO TOWAR VALUES  (102, 'Majtki meskie XXL', 3, 7.99, 3, 5 ); 
-INSERT INTO TOWAR VALUES  (103, 'Ibum', 4, 8.15, 4, 12 ); 
-INSERT INTO TOWAR VALUES  (104, 'Karma dla kota', 5, 9.15, 5, 20 ); 
-INSERT INTO TOWAR VALUES  (105, 'LM Blue', 6, 14.30 , 4, 12 ); 
+INSERT INTO TOWAR VALUES  (100, 'Zel pod prysznic', 1, 1.99,10); 
+INSERT INTO TOWAR VALUES  (101, 'Cif', 2, 2.99, 20 ); 
+INSERT INTO TOWAR VALUES  (102, 'Majtki meskie XXL', 3, 7.99, 5 ); 
+INSERT INTO TOWAR VALUES  (103, 'Ibum', 4, 8.15, 12 ); 
+INSERT INTO TOWAR VALUES  (104, 'Karma dla kota', 5, 9.15,20 ); 
+INSERT INTO TOWAR VALUES  (105, 'LM Blue', 6, 14.30 , 12 ); 
 
 
 --Pracownik    idP          Imie         Nazwisko    Dzial      Pensja 
@@ -94,30 +111,32 @@ INSERT INTO Pracownik VALUES (4, 'Iwona', 'Malak', 4, 2000);
 INSERT INTO Pracownik VALUES (5, 'Joanna', 'Grzyb', 5, 2000);  
 INSERT INTO Pracownik VALUES (6, 'Barbara', 'Stawska', 6, 2000);  
 
---Transakcja  idTra Towar cena_jednostkowa Ilosc  Suma  Sprzedawca  Opinia 
-INSERT INTO TRANSAKCJA VALUES (100, 100, 1.99, 1, 1.99,1,1);  
-INSERT INTO TRANSAKCJA VALUES (101, 101, 2.99, 1, 2.99,1,1);  
-INSERT INTO TRANSAKCJA VALUES (102, 102, 7.99, 1, 7.99,1,1);  
-INSERT INTO TRANSAKCJA VALUES (103, 103, 8.15, 1, 8.15,1,1);  
-INSERT INTO TRANSAKCJA VALUES (104, 104, 9.15, 1, 9.15,1,1);  
-INSERT INTO TRANSAKCJA VALUES (105, 105, 14.30, 1, 14.30,1,1);  
+--Transakcja  idTra Towar cena_jednostkowa Ilosc  Suma  Sprzedawca 
+INSERT INTO TRANSAKCJA VALUES (100, 100, 1.99, 1, 1.99,1);  
+INSERT INTO TRANSAKCJA VALUES (101, 101, 2.99, 1, 2.99,1);  
+INSERT INTO TRANSAKCJA VALUES (102, 102, 7.99, 1, 7.99,1);  
+INSERT INTO TRANSAKCJA VALUES (103, 103, 8.15, 1, 8.15,1);  
+INSERT INTO TRANSAKCJA VALUES (104, 104, 9.15, 1, 9.15,1);  
+INSERT INTO TRANSAKCJA VALUES (105, 105, 14.30, 1, 14.30,1);  
 
 --Opinia  idO    Ocena   Imie  Nazwisko   Tresc   
-INSERT INTO OPINIA VALUES (1,5,'Janusz','Kowalski','Najlepszy sklep na ');  
-INSERT INTO OPINIA VALUES (2,5,'Piotr','Salapata','Lubie ten sklep');  
-INSERT INTO OPINIA VALUES (3,4,'Kinga','Wawrzyniak','Jest ok');  
-INSERT INTO OPINIA VALUES (4,3,'Bartosz','Galecki','Dlugo stalem w kolejce');  
-INSERT INTO OPINIA VALUES (5,5,'Janina','Woods','Wszystko czego potrzebuje');  
-INSERT INTO OPINIA VALUES (6,1,'Anna','Warchol','Nic nie ma w tym sklepie');  
+INSERT INTO OPINIA VALUES (1,5,'Janusz','Kowalski','Super ¯el',100);  
+INSERT INTO OPINIA VALUES (2,5,'Piotr','Salapata','Cif taki jak zawsze',101);  
+INSERT INTO OPINIA VALUES (3,4,'Kinga','Wawrzyniak','Idealne majtki',102);  
+INSERT INTO OPINIA VALUES (4,3,'Bartosz','Galecki','Coœ nie dzialaja te tableki',103);  
+INSERT INTO OPINIA VALUES (5,5,'Janina','Woods','Kotek jest przeszczêsliwy',104);  
+INSERT INTO OPINIA VALUES (6,1,'Anna','Warchol','Drogie te fajki',105);  
 
   
   
-ALTER TABLE Dzial ADD CONSTRAINT fk_dzial FOREIGN KEY (Kierownik) REFERENCES Pracownik(idP) ;
+--ALTER TABLE Dzial ADD CONSTRAINT fk_dzial FOREIGN KEY (Kierownik) REFERENCES Pracownik(idP) ;
 ALTER TABLE Towar ADD CONSTRAINT fk_towar FOREIGN KEY (dzial) REFERENCES dzial(idD) ;
-ALTER TABLE Towar ADD CONSTRAINT fk_towar2 FOREIGN KEY (opinia) REFERENCES opinia(idO) ;
+--ALTER TABLE Towar ADD CONSTRAINT fk_towar2 FOREIGN KEY (opinia) REFERENCES opinia(idO) ;
+ALTER TABLE Opinia ADD CONSTRAINT fk_opinia FOREIGN KEY (idT) REFERENCES Towar(idT) ;
 ALTER TABLE Pracownik ADD CONSTRAINT fk_pracownik FOREIGN KEY (dzial) REFERENCES dzial(idD) ;
 ALTER TABLE Transakcja ADD CONSTRAINT fk_transakcja FOREIGN KEY (towar) REFERENCES towar(idT);
 ALTER TABLE Transakcja ADD CONSTRAINT fk_transakcja_1 FOREIGN KEY (Sprzedawca) REFERENCES Pracownik(idP);
+
 
 CREATE OR REPLACE  TRIGGER trigger_towar
 BEFORE INSERT OR UPDATE
@@ -138,6 +157,8 @@ then
 end if;
 end;
 
+/
+
 CREATE OR REPLACE  TRIGGER trigger_pracownik
 BEFORE INSERT OR UPDATE
   ON PRACOWNIK
@@ -156,6 +177,8 @@ then
   (SYSDATE,'aktualizacja danych pracownika');
 end if;
 end;
+
+/
 
 CREATE OR REPLACE  TRIGGER trigger_dzial
 BEFORE INSERT OR UPDATE
@@ -176,6 +199,8 @@ then
 end if;
 end;
 
+/
+
 CREATE OR REPLACE  TRIGGER trigger_transakcja
 BEFORE INSERT OR UPDATE
   ON transakcja
@@ -194,6 +219,8 @@ then
   (SYSDATE,'aktualizacja transakcji');
 end if;
 end;
+
+/
 
 CREATE OR REPLACE  TRIGGER trigger_opinia
 BEFORE INSERT OR UPDATE
@@ -214,6 +241,10 @@ then
 end if;
 end;
 
+--test trigera
+--select * from USER_TRIGGERS;
+--UPDATE TOWAR SET Cena=2.99 WHERE idT=100;
+--select * from log;
 
 
 
